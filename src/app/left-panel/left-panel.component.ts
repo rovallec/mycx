53bd97641881cd2c0c73544aff7fa23585b7f85f
rovallec <56641456+rovallec@users.noreply.github.com>
2024-09-29 22:55:09 -0600

    images

--- src/app/left-panel/left-panel.component.ts
+++ src/app/left-panel/left-panel.component.ts
@@ -1,13 +1,33 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { profiles } from '../profile';
 @Component({
   selector: 'app-left-panel',
   templateUrl: './left-panel.component.html',
-  styleUrl: './left-panel.component.css'
+  styleUrls: ['./left-panel.component.css'], // Change `styleUrl` to `styleUrls`
 })
-export class LeftPanelComponent {
-
+export class LeftPanelComponent implements OnInit {
   activeItem: string = 'profile'; // Default active item
+  profilePhoto: string | null = null; // Variable to hold the photo URL
+  profile!: profiles;
+
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    // Fetch user profile photo as a Blob
+    this.http.get('https://graph.microsoft.com/v1.0/me/photo/$value', { responseType: 'blob' })
+      .subscribe(photoBlob => {
+        // Create a URL for the Blob
+        const photoUrl = URL.createObjectURL(photoBlob);
+        this.profilePhoto = photoUrl; // Store the URL for use in the template
+      });
+
+      this.http.get<profiles>('https://graph.microsoft.com/v1.0/me')
+      .subscribe(profile => {
+        this.profile = profile;
+      });
+  }
 
   setActive(item: string): void {
     this.activeItem = item;
@@ -17,5 +37,4 @@ export class LeftPanelComponent {
   isActive(item: string): boolean {
     return this.activeItem === item;
   }
-
 }
--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,27 +1,34 @@
 import { Component, OnInit } from '@angular/core';
-
 import { HttpClient } from '@angular/common/http';
-
-import { profiles } from '../profile'
+import { profiles } from '../profile';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrl: './profile.component.css'
+  styleUrls: ['./profile.component.css'], // Fix here: Change `styleUrl` to `styleUrls`
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
   profile!: profiles;
   tokenExpiration!: string;
+  profilePhoto: string | null = null; // Change to string to hold the image URL
 
-  constructor(
-    private http: HttpClient
-  ) { }
+  constructor(private http: HttpClient) {}
 
   // When the page loads, perform an HTTP GET request from the Graph /me endpoint
   ngOnInit() {
-    this.http.get('https://graph.microsoft.com/v1.0/me')
+    // Fetch user profile
+    this.http.get<profiles>('https://graph.microsoft.com/v1.0/me')
       .subscribe(profile => {
         this.profile = profile;
+      });
+
+    // Fetch user profile photo as a Blob
+    this.http.get('https://graph.microsoft.com/v1.0/me/photo/$value', { responseType: 'blob' })
+      .subscribe(photoBlob => {
+        // Create a URL for the Blob
+        const photoUrl = URL.createObjectURL(photoBlob);
+        this.profilePhoto = photoUrl; // Store the URL for use in the template
+        console.log(this.profilePhoto);
       });
 
     this.tokenExpiration = localStorage.getItem('tokenExpiration')!;
